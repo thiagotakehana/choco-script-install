ebc16118992a0c3cccd25ad167bf1d83f80bc8ff
Takehana <thiagoyuiti@yahoo.com.br>
2019-08-03 21:22:35 -0300

    Commit do arquivo de instalacao

---
+++ install_aps.ps1
@@ -0,0 +1,76 @@
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+# Script de instalação programas	#
+# script by Takehana				#
+# v0.0.1 03/08/2019					#
+#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#
+
+#---------------------
+# DEV TOOLS
+#---------------------
+
+# Visual Studio Code
+choco install vscode -y
+
+# ILSPY decompiler
+choco install ilspy -y
+
+# SQL Management Studio
+choco install sql-server-management-studio -y
+
+# SVN Tortoise
+choco install tortoisesvn -y
+
+# Git
+choco install git -y
+
+# Node js
+choco install nodejs -y
+
+# NuGet Package Explorer
+choco install nugetpackageexplorer -y
+
+# WinSCP
+choco install winscp -y
+
+# Postman
+choco install postman -y
+
+#-----------------------
+# Ferramentas auxiliares
+#-----------------------
+
+# Notepad++
+choco install notepadplusplus -y
+
+# Open VPN
+choco install openvpn -y
+
+# Foxit reader
+choco install foxitreader -y
+
+# 7 zip
+choco install 7zip -y
+
+# Google Chrome
+choco install googlechrome -y
+
+# Firefox
+choco install firefox -y
+
+# Dropbox
+choco install dropbox -y
+
+# Google Play Music Desktop (Unofficial)
+choco install gpmdp -y
+
+# Paint.net
+choco install paint.net -y
+
+# WindirStat (Explorador de arquivos)
+choco install windirstat -y
+
+# Office 365 proplus
+# todo configurar instalacao com apps customizado e em portugues
+
+# Microsoft teams
+choco install microsoft-teams -y
